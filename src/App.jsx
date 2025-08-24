471878046a26f79cf7e6a50f685ee389e616e486
Mariano Juarez <cm.juarez.95@gmail.com>
2025-08-24 17:18:43 -0300

    Primer commit limpiamos el proyecto de react e instalamos dependencias

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <main className='container'>
+      <h1>Hola mundo</h1>
+    </main>
+      
+    </>
+  )
+}
+
+export default App
