4c0640c225a0614b76d51d70d4c3fd5e7b404ab4
Mariano Juarez <cm.juarez.95@gmail.com>
2025-08-24 20:38:48 -0300

    se ajusto un detalle visual

--- src/components/EmpleadoAvatar.jsx
+++ src/components/EmpleadoAvatar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Figure } from 'react-bootstrap';
 const EmpleadoAvatar = (empleado) => {
     return (
-        <Figure className='me-2'><Figure.Image
+        <Figure className='me-2 mb-0' ><Figure.Image
         width={100}
         height={100}
         alt="171x180"
--- src/components/EmpleadoRow.jsx
+++ src/components/EmpleadoRow.jsx
@@ -4,7 +4,7 @@ import EmpleadoAvatar from './EmpleadoAvatar';
 
 const EmpleadoRow = ({empleado}) => {
     return (
-        <ListGroupItem className='d-flex'><EmpleadoAvatar pic={empleado.pic}></EmpleadoAvatar>
+        <ListGroupItem className='d-flex '><EmpleadoAvatar pic={empleado.pic}></EmpleadoAvatar>
         <div className='d-flex flex-column justify-content-around'>
             <h5>{empleado.fullName}</h5>
             <small className="text-muted">{empleado.title} <Badge bg="primary">{empleado.department}</Badge></small>
